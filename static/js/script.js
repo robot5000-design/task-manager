3db74fad4baa038bc3f92b265de598e6a4c2cf77
robot5000-design <ducativtwin@gmail.com>
2021-02-09 00:18:31 +0000

    Bind data from db to accordian

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('input#username, input#password').characterCounter();
     $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
 });
