97fe00be06fb830a626babdb15b44c7604afa9fc
robot5000-design <ducativtwin@gmail.com>
2021-02-09 18:59:40 +0000

    Add new tasks form inputs and style

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
     $('input#username, input#password').characterCounter();
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
