5621c2846ead026cb9fa3d09c9614b834d576d67
robot5000-design <ducativtwin@gmail.com>
2021-02-16 23:21:34 +0000

    Added add_category form and post data to db

--- app.py
+++ app.py
@@ -158,6 +158,27 @@ def delete_task(task_id):
     return redirect(url_for("get_tasks"))
 
 
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    # sends the data from the form when submit button is pressed
+    if request.method == "POST":
+        category = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.insert_one(category)
+        flash("New Category Added")
+        return redirect(url_for("get_categories"))
+
+    # get or present the form page when button is pressed from categories page
+    return render_template("add_category.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
